f3edb145d52cdd9d5597253265f6dd750932df28
Nishchhal <kumarnishchhal175@gmail.com>
2020-09-25 23:26:50 +0530

    Now Full working app Date: 25/09/2020 what left is designing

--- app/src/main/java/com/example/quickmeals/SelectMenuItem.kt
+++ app/src/main/java/com/example/quickmeals/SelectMenuItem.kt
@@ -6,15 +6,21 @@ import android.widget.ExpandableListAdapter
 import android.widget.ExpandableListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.iterator
-import com.example.quickmeals.ExpandableListData.data
+import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.List
+import kotlin.collections.MutableList
+import kotlin.collections.mutableListOf
+import kotlin.collections.set
 
 class SelectMenuItem : AppCompatActivity() {
     private var expandableListView: ExpandableListView? = null
     private var adapter: ExpandableListAdapter? = null
     private var titleList: List<String>? = null
+    val menuLast = HashMap<String, List<String>>()
     val db = Firebase.firestore
     var listOfDish: MutableList<String> = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,59 +28,62 @@ class SelectMenuItem : AppCompatActivity() {
         setContentView(R.layout.activity_select_menu_item)
         title = "Add To Your Cart"
         expandableListView = findViewById(R.id.expendableList)
-        if (expandableListView != null) {
-            val listData = data
-            titleList = ArrayList(listData.keys)
-            adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
-            expandableListView!!.setAdapter(adapter)
+        val shopi = intent.getStringExtra("cliked") as String
+        val gmenu = db.collection("resto").document(shopi)
+        val source = Source.CACHE
+        gmenu.get(source).addOnCompleteListener{ task ->
+            if(task.isSuccessful) {
+                val d = task.result
+                listOfDish = d?.get("menu") as MutableList<String>
+                for (ld in listOfDish){
+                    db.collection("resto/$shopi/$ld")
+                        .get()
+                        .addOnSuccessListener {
+                            val mutMlist = mutableListOf<String>()
+                            for (docu in it){
+                                mutMlist.add(docu.id)
+                            }
+                            menuLast.put(ld,mutMlist)
+                            if (expandableListView != null) {
+                                titleList = ArrayList(menuLast.keys)
+                                adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, menuLast)
+                                expandableListView!!.setAdapter(adapter)
 
-            expandableListView!!.setOnGroupExpandListener { groupPosition ->
-                Toast.makeText(
-                    applicationContext,
-                    (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
-                Toast.makeText(
-                    applicationContext,
-                    (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            expandableListView!!.setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
-                Toast.makeText(
-                    applicationContext,
-                    "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(
-                            titleList as
-                                    ArrayList<String>
-                            )
-                            [groupPosition]]!!.get(
-                        childPosition
-                    ),
-                    Toast.LENGTH_SHORT
-                ).show()
-                false
-            }
-            readData(){
-                Log.d("impiS", it.size.toString() + it.toString())
+                                expandableListView!!.setOnGroupExpandListener { groupPosition ->
+                                    Toast.makeText(
+                                        applicationContext,
+                                        (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                                expandableListView!!.setOnGroupCollapseListener { groupPosition ->
+                                    Toast.makeText(
+                                        applicationContext,
+                                        (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                                expandableListView!!.setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + menuLast[(
+                                                titleList as
+                                                        ArrayList<String>
+                                                )
+                                                [groupPosition]]!!.get(
+                                            childPosition
+                                        ),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    false
+                                }
+                            }
+                        }
+                }
+
+            }else{
+                Log.d("error", "data not found")
             }
         }
-    }
-    fun readData(myCallback: (List<String>) -> Unit) {
-        val shopName = intent.getStringExtra("cliked") as String
-        Log.d("impiS", "resto/$shopName")
-        db.collection("resto/$shopName")
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    Log.d("selectMenu", document.id)
-                    listOfDish.add(document.id)
-                }
-                myCallback(listOfDish)
-            }
-            .addOnFailureListener { exception ->
-                Log.w("findxe", "Error getting documents: ", exception)
-            }
     }
 }
